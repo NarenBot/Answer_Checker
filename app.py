c953a933e8fd4018313dda196b09829e96715ece
Naren <narendas10@gmail.com>
2023-03-21 08:54:30 +0530

    Portal updated

---
+++ app.py
@@ -0,0 +1,136 @@
+from flask import Flask, render_template, request, jsonify
+import boto3
+from werkzeug.utils import secure_filename
+from trp import Document
+import os
+
+from utils import inputDataExcel
+
+
+app = Flask(__name__)
+
+ACCESS_KEY_ID = "AKIAQULFVLB72WTFGQXJ"
+ACCESS_SECRET_KEY = "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"
+BUCKET_NAME = "narenineuron"
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+)
+
+# Amazon Textract client
+textract = boto3.client(
+    "textract",
+    aws_access_key_id=ACCESS_KEY_ID,
+    aws_secret_access_key=ACCESS_SECRET_KEY,
+    region_name="ap-south-1",
+)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["post"])
+def upload():
+    if request.method == "POST":
+        img = request.files["file"]
+        if img:
+            filename = secure_filename(img.filename)
+            img.save("images/temp/" + filename)
+            s3.upload_file(
+                Bucket=BUCKET_NAME, Filename="images/" + filename, Key=filename
+            )
+            msg = f"{filename} uploaded into S3 Bucket! "
+        else:
+            msg = "Please choose the file above and click the upload button."
+
+    return render_template("index.html", msg=msg)
+
+
+@app.route("/delete")
+def delete():
+    objects = s3.list_objects_v2(Bucket=BUCKET_NAME)
+    # print(objects["KeyCount"])
+
+    if objects["KeyCount"] != 0:
+        ls = []
+        for obj in objects["Contents"]:
+            ls.append(obj["Key"])
+        for filename in ls:
+            s3.delete_object(Bucket=BUCKET_NAME, Key=filename)
+
+        msg = f"Objects have been deleted!"
+    else:
+        msg = f"Fine! Now you can upload your file."
+
+    return render_template("index.html", msg=msg)
+
+
+@app.route("/extract", methods=["post"])
+def extract():
+    if request.method == "POST":
+        objects = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        # print(objects["KeyCount"])
+
+        if objects["KeyCount"] != 0:
+            ls = []
+            for obj in objects["Contents"]:
+                ls.append(obj["Key"])
+
+            text = []
+            for filename in ls:
+                response = textract.detect_document_text(
+                    Document={"S3Object": {"Bucket": BUCKET_NAME, "Name": filename}}
+                )
+
+                for item in response["Blocks"]:
+                    if item["BlockType"] == "LINE":
+                        # print("\033[92m" + item["Text"] + "\033[92m")
+                        # print(item["Text"])
+                        text.append(item["Text"])
+
+            answer = " ".join(text)
+
+            return render_template("index.html", text=answer)
+        else:
+            return render_template(
+                "index.html",
+                msg="Please upload any image into S3 and click extract button",
+            )
+
+
+@app.route("/evaluate", methods=["GET"])
+def evaluate():
+    if request.method == "GET":
+        objects = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        if objects["KeyCount"] != 0:
+            ls = []
+            for obj in objects["Contents"]:
+                ls.append(obj["Key"])
+
+            text = []
+            for filename in ls:
+                response = textract.detect_document_text(
+                    Document={"S3Object": {"Bucket": BUCKET_NAME, "Name": filename}}
+                )
+                for item in response["Blocks"]:
+                    if item["BlockType"] == "LINE":
+                        # print("\033[92m" + item["Text"] + "\033[92m")
+                        # print(item["Text"])
+                        text.append(item["Text"])
+            answer = " ".join(text).lower()
+            id = int(answer[:2]) - 23
+            print(id)
+
+        inputDataExcel(answer=answer, id=id)
+
+    return render_template(
+        "index.html", msg="Evaluation Completed. Please check the report."
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,40 @@
+import base64
+import requests
+import json
+from openpyxl import Workbook, load_workbook
+
+
+# answer = "25) every village or group of villages has a gram panchayat which consists of severall ward members c panch_] and a presstr(id)ent [sarpanch]. it is elected by all the adult ward members living in that ward it is under the supervision on of the gram sabha which consists of the adult members of that population who meet a few times a year to discuss the performance of the gram sabha and the annual budget. a few gram sabhas form a panchayat samiti or block or mandal elected by all the panchayat members in that area a few panchayat samitis constitute the zilla parishad which 13 consists of mlas members of legislative assemblies, and other ministers. zilla parishad chairperson is the political head of zilla parishad"
+
+
+def inputDataExcel(answer, id):
+    countWords = len(answer)
+    wb = load_workbook("Answers-Hint.xlsx")
+    ws = wb.active
+    ws["E" + str(id)].value = answer
+    keywords = ws["D" + str(id)].value
+    keywords = keywords.split(",")
+
+    if countWords >= 100:
+        ws["F" + str(id)].value = 5
+    else:
+        ws["F" + str(id)].value = 0
+
+    count = 0
+    for i in keywords:
+        if i in answer:
+            count += 1
+            if 1 <= count < 3:
+                ws["G" + str(id)].value = 2
+            elif count >= 3:
+                ws["G" + str(id)].value = 5
+        else:
+            ws["G" + str(id)].value = 0
+
+    totalMarks = ws["F" + str(id)].value + ws["G" + str(id)].value
+    ws["H" + str(id)].value = totalMarks
+
+    wb.save("Answers-Hint.xlsx")
+
+
+# inputDataExcel(answer=answer, id=answer[:1])
